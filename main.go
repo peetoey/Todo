ffa7f21e4a827a05b17f50b27bc5d8b5006b6e4c
Saksit Jantaraplin <saksit.ja@kbtg.tech>
2020-04-13 03:50:20 +0700

    Refactor

--- main.go
+++ main.go
@@ -16,63 +16,75 @@ type Todo struct {
 	Description string
 }
 
-var templates = template.Must(template.ParseFiles("index.html"))
+var (
+	db        *sql.DB
+	err       error
+	templates = template.Must(template.ParseFiles("index.html"))
+)
 
 func main() {
-
+	err = dbConn()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	http.HandleFunc("/", todo)
 	http.HandleFunc("/delete", delete)
 	http.ListenAndServe(":8080", nil)
 }
 
 func todo(res http.ResponseWriter, req *http.Request) {
-	db, err := sql.Open("postgres", "postgres://zecolkej:KwZAmgZmsXEN_xPhnzWEsNmNRz2rNlJl@john.db.elephantsql.com:5432/zecolkej")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 
+	// Query fro db
 	rows, err := db.Query("SELECT id, subject, description FROM todo")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	resp := Todo{}
-	var results []Todo
+	todo := Todo{}
+	var resp []Todo
 
+	// Read data increase to array
 	for rows.Next() {
 		var id int
 		var sj, des string
 		err = rows.Scan(&id, &sj, &des)
-		resp.ID = id
-		resp.Subject = sj
-		resp.Description = des
+		todo.ID = id
+		todo.Subject = sj
+		todo.Description = des
 
-		results = append(results, resp)
+		resp = append(resp, todo)
 
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	templates.Execute(res, results)
+	// Render templates
+	templates.Execute(res, resp)
 }
 
 func delete(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("begin delete")
-	db, err := sql.Open("postgres", "postgres://zecolkej:KwZAmgZmsXEN_xPhnzWEsNmNRz2rNlJl@john.db.elephantsql.com:5432/zecolkej")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
 
+	// Get key from url
 	key := req.URL.Query().Get("ID")
 
+	// Delete from db
 	_, err = db.Exec(fmt.Sprintf("DELETE FROM todo WHERE id=%s", key))
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("deleted")
 
+	// Redirect to main page
 	http.Redirect(res, req, "/", 301)
+}
+
+func dbConn() error {
+	db, err = sql.Open("postgres", "postgres://zecolkej:KwZAmgZmsXEN_xPhnzWEsNmNRz2rNlJl@john.db.elephantsql.com:5432/zecolkej")
+	if err != nil {
+		return err
+	}
+	return nil
 }
