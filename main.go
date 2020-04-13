26caf6d9401501243fe56fccc6c4dd6d1fe134b0
Saksit.Ja <e29dqt@gmail.com>
2020-04-13 10:12:19 +0700

    Add main page

--- main.go
+++ main.go
@@ -3,25 +3,76 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
-var (
-	db *sql.DB
-)
-
-func main() {
-	connectDB()
+type Todo struct {
+	ID          int
+	Subject     string
+	Description string
 }
 
-func connectDB() {
-	var err error
-	connectionString := "postgres://zecolkej:KwZAmg...@john.db.elephantsql.com:5432/zecolkej"
-	conn, err := sql.Open("postgres", connectionString)
+var templates = template.Must(template.ParseFiles("index.html"))
+
+func main() {
+
+	http.HandleFunc("/", todo)
+	http.HandleFunc("/delete", delete)
+	http.ListenAndServe(":8080", nil)
+}
+
+func todo(res http.ResponseWriter, req *http.Request) {
+	db, err := sql.Open("postgres", "postgres://zecolkej:KwZAmgZmsXEN_xPhnzWEsNmNRz2rNlJl@john.db.elephantsql.com:5432/zecolkej")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Connect DB Success\n", conn)
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, subject, description FROM todo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp := Todo{}
+	var results []Todo
+
+	for rows.Next() {
+		var id int
+		var sj, des string
+		err = rows.Scan(&id, &sj, &des)
+		resp.ID = id
+		resp.Subject = sj
+		resp.Description = des
+
+		results = append(results, resp)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	templates.Execute(res, results)
+}
+
+func delete(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("begin delete")
+	db, err := sql.Open("postgres", "postgres://zecolkej:KwZAmgZmsXEN_xPhnzWEsNmNRz2rNlJl@john.db.elephantsql.com:5432/zecolkej")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	key := req.URL.Query().Get("ID")
+
+	_, err = db.Exec(fmt.Sprintf("DELETE FROM todo WHERE id=%s", key))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("deleted")
+
+	http.Redirect(res, req, "/", 301)
 }
